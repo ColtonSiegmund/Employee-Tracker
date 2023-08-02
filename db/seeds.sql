31584bad03640d79a463298942961980ad1e3b2b
ColtonSiegmund <135175571+ColtonSiegmund@users.noreply.github.com>
2023-08-02 16:00:36 -0500

    updated inquirer and wrote sql files

---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (name)
+VALUES ("Front End"),
+       ("Back End"),
+       ("Java"),
+       ("NODE"),
+       ("MySQL");
+
+INSERT INTO role (title, salary)
+VALUES ("Web Dev", 800000),
+       ("Web Dev2", 800001),
+       ("Web Dev3", 800002),
+       ("Web Dev4", 800003),
+       ("Web Dev5", 800004);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("John", "Doe"),
+       ("John2", "Doe2"),
+       ("John3", "Doe3"),
+       ("John4", "Doe4"),
+       ("John5", "Doe5");
--- index.js
+++ index.js
@@ -1,62 +1,80 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const mysql = require('mysql2');
 
-function init(){
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '11111111',
+  database: 'employees_db',
+});
+
+appStart = () => {
+  console.log("Welcome to Employee Manager");
+  firstPrompt()
+}
+
+function firstPrompt(){
   inquirer
   .createPromptModule([
     {
-      type: 'checkbox',
-      message:'Which would you like to add?',
-      name: 'choices',
-      choices: ["department", "role", "employee"]
-    },
-  ])
-  if (choices === "department"){
-      inquirer
-      .createPromptModule([
+      type: 'select',
+      message:'Which would you like to do?',
+      choices: [
         {
-          type: 'input',
-          message: 'What is your department name?',
-          name: 'department'
-        }
-      ])
-  } else if (choices === "role"){
-    inquirer
-    .createPromptModule([
-      {
-        type: 'input',
-        message: 'What is the title of the role?',
-        name: 'roleName'
-      },
-      {
-        type: 'input',
-        message: 'What is the salary of the role?',
-        name: 'roleSalary'
-      }
-    ])
-  } else {
-    inquirer
-    .createPromptModule([
-      {
-        type: 'input',
-        message: 'What is the employees first name?',
-        name: 'employeeFirstName'
-      },
-      {
-        type: 'input',
-        message: 'What is the employees last name?',
-        name: 'employeeLastName'
-      },
-      {
-        type: 'input',
-        message: 'What is the employees role?',
-        name: 'employeeRole'
-      },
-      {
-        type: 'input',
-        message: 'Who is the employees manager?',
-        name: 'employeeManager'
-      }
-    ])
-  }
+          name:"View All Departments",
+          value: "VIEW_ALL_DEPARTMENT"
+        },
+        {
+          name: "View All Roles",
+          value: "VIEW_ALL_ROLES"
+          },
+          {
+          name: "View All Employees",
+          value: "VIEW_ALL_EMPLOYEE"
+          },
+          {
+          name: "Add a department",
+          value: "ADD_DEPARTMENT"
+          },
+          {
+            name: "Add a role",
+            value: "ADD_ROLE"
+          },
+          {
+            name: "Add an employee",
+            value: "ADD_EMPLOYEE"
+          },
+          {
+            name: "Update employee role",
+            value: "UPDATE_EMPLOYEE"
+          } 
+        ]
+    },
+  ]).then (res => {
+    let choice = res.choice;
+    switch (choice) {
+      case "VIEW_ALL_DEPARTMENT":
+        viewDepartments();
+        break;
+        case "VIEW_ALL_ROLES":
+          viewRoles();
+          break;
+          case "VIEW_ALL_EMPLOYEE":
+            viewEmployee();
+            break;
+            case "ADD_DEPARTMENT":
+              addDepartment();
+              break;
+              case "ADD_ROLE":
+                addRole();
+                break;
+                case "ADD_EMPLOYEE":
+                  addEmployee();
+                  break;
+                  case "UPDATE_EMPLOYEE":
+                    updateEmployee();
+                    break;
+    }
+  })
 }
